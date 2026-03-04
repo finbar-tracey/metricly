2db3326bab0656a0eeaf54760d5cafcedfb053f1
Finbar Tracey <Finbartracey@gmail.com>
2026-03-04 22:25:40 +0000

    Initial Commit

---
+++ tracker/trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  trackerApp.swift
+//  tracker
+//
+//  Created by Finbar Tracey on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
